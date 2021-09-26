f3f87f16ddce45a6fbf1f9229b82670a1f689530
Adam Finlayson <amf@adamfinlayson.com>
2021-09-25 23:28:08 -0500

    feat: constants

---
+++ src/util/constants.js
@@ -0,0 +1,23 @@
+//
+//
+// Constants
+//
+
+const TYPE = {
+  ANY: "*",
+  ARRAY: Array,
+  FUNCTION: Function,
+  NUMBER: Number,
+  NULL: null,
+  OBJECT: Object,
+  STRING: String,
+};
+
+//
+//
+// Export
+//
+
+module.exports = {
+  TYPE,
+};
