2792157e9e8dbf673c2c1fd98f7dc3074349855f
Adam Finlayson <amf@adamfinlayson.com>
2021-09-27 14:03:21 -0500

    feat: add constants

--- src/util/constants.js
+++ src/util/constants.js
@@ -6,11 +6,13 @@
 const TYPE = {
   ANY: "*",
   ARRAY: Array,
+  CLASS: "_CLASS",
   FUNCTION: Function,
   NUMBER: Number,
   NULL: null,
   OBJECT: Object,
   STRING: String,
+  UNDEFINED: "_UNDEFINED",
 };
 
 //
